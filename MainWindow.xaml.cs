0ca0ba7a3a071bb0cd2288f9e109a16771eac31f
super mario <mario160@mail.ru>
2022-07-21 13:12:17 +0300

    Working proof of concept

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,44 @@ namespace Windows_Google_Lens
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const String lensUrl = "https://lens.google.com/upload";
+        private readonly HttpClient httpClient;
+        private readonly Regex resultUrlRegex;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            httpClient = new HttpClient();
+            resultUrlRegex = new Regex(
+                @"https:\/\/lens\.google\.com\/search\?p=[^\""]+",
+                RegexOptions.Compiled);
+        }
+
+        private async void debugButton_Click(object sender, RoutedEventArgs e)
+        {
+            MultipartFormDataContent formData = new MultipartFormDataContent();
+            ByteArrayContent encoded_image = new ByteArrayContent(
+                File.ReadAllBytes("C:/buff/img.jpg"));
+            encoded_image.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+            formData.Add(encoded_image, "encoded_image");
+
+            String queryString = await new FormUrlEncodedContent(
+                new Dictionary<String, String>{
+                    { "ep", "ccm" },
+                    { "s", "csp" },
+                    { "st", DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString() }
+                }
+            ).ReadAsStringAsync();
+
+            HttpResponseMessage response = await httpClient.PostAsync(
+                $"{lensUrl}?{queryString}", formData);
+            response.EnsureSuccessStatusCode();
+            String response_text = await response.Content.ReadAsStringAsync();
+
+            MatchCollection matches = resultUrlRegex.Matches(response_text);
+
+            System.Diagnostics.Process.Start(matches[0].Value);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# Windows Google Lens
+
+Project to make native port of mobile version of Google Lense onto Windows.
+
+The concept is firstly presented by https://github.com/WhoIsJayD/Google-Lens-For-Windows
+
+## The difficult part of project
+
+The basis of the project is the Snipping Tool, that allows user to send screenshot to the Google Lens.
+To make it work the UWP APIs were used:
+
+* How-to guide: https://stackoverflow.com/questions/33692898/is-it-possible-to-use-uwp-apis-in-a-wpf-app
+* Docs: https://github.com/MicrosoftDocs/windows-uwp/blob/docs/windows-apps-src/launch-resume/launch-screen-snipping.md
